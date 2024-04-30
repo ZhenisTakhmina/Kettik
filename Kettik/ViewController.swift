149cf1142b7787ce723e552c13404bda1ba68d4f
Murzatayev Ruslan <mrmurqa@gmail.com>
2024-04-30 23:10:52 +0500

    Initial Commit

---
+++ Kettik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kettik
+//
+//  Created by Ruslan on 30.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
